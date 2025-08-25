4b4d00ee03f23a5733d61a70076731f46e8eb5fa
GorillaSpring <BenLevy3@gmail.com>
2025-08-25 13:27:33 -0700

    changing class names

---
+++ src/main/java/com/bosco/stdata/model/School.java
@@ -0,0 +1,24 @@
+package com.bosco.stdata.model;
+
+
+import lombok.Data;
+
+@Data
+public class School {
+
+    private String districtId;
+
+	/*
+	 * PRE, PRIMARY, MIDDLE, SECONDARY (, TERTIARY)
+	 */
+	private String level;
+
+    // WE DO NOT GET FOR Uplift or Celina.
+	/*
+	 * Different schools can have different grades in their levels
+	 */
+	private String grades;
+
+    private String name;
+
+}
