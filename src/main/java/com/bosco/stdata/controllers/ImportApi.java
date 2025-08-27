42891479bb91d988575b5bb7b0dd2f8c17a80e54
GorillaSpring <BenLevy3@gmail.com>
2025-08-27 09:40:04 -0700

    added in email service.

--- src/main/java/com/bosco/stdata/controllers/ImportApi.java
+++ src/main/java/com/bosco/stdata/controllers/ImportApi.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bosco.stdata.repo.ImportRepo;
+import com.bosco.stdata.service.EmailService;
 import com.bosco.stdata.tasks.ImportTask;
 import com.bosco.stdata.utils.ImportHelper;
 
@@ -21,7 +22,10 @@ public class ImportApi {
     @Autowired 
     ImportTask importTask;
 
-    
+    @Autowired
+    EmailService emailService;
+
+
     @GetMapping("/import/runImport")
     public String runImportNow() {
         //ImportTask importTask = new ImportTask();
@@ -32,6 +36,13 @@ public class ImportApi {
 
     }
 
+
+    @GetMapping("/import/testEmail")
+    public String getMethodName() {
+        emailService.sendSimpleMessage("benlevy3@gmail.com", "Tesing the email in bosco-stdata with props", "This is the email we are sending with props");
+        return "Sent";
+    }
+    
 
     @GetMapping("/test/testFilesExists")
     public String testFilesExists() {
