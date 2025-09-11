acf36818d72792aaf9d85578e8570191bee6fed9
GorillaSpring <BenLevy3@gmail.com>
2025-09-11 12:43:21 -0700

    got UpLift in place refactored mySql.  back to TEA files

---
+++ src/main/java/com/bosco/stdata/teaModel/Star2024.java
@@ -0,0 +1,32 @@
+package com.bosco.stdata.teaModel;
+
+import lombok.Data;
+
+@Data
+public class Star2024 {
+     private String adminDate;
+     private String studentId;
+
+     private String scoreReadingLanguageArts;
+     private String pliReadingLanguageArts;
+
+
+
+     private String scoreMath;
+     private String pliMath;
+
+
+     private String scoreSocalStudies;
+     private String pliSocalStudies;
+
+
+     private String scoreScience;
+     private String pliScience;
+
+     private String scoreAlgebra;
+
+
+
+
+     private String throwaway;
+}
